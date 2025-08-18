e688c84ce31c90b4696c26e4241ac55132ceb401
ashley-joseph <ashleyjoseph01@proton.me>
2025-08-17 21:39:46 -0600

    modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,8 +1,8 @@
 console.log("Starting JS!")
 
 const modal = document.querySelector('#project-modal');
-const openModal = document.querySelector('.open-button');
-const closeModal = document.querySelector('.close-button');
+const openModal = document.querySelector(".open-button");
+const closeModal = document.querySelector("dialoge button");
 
 openModal.addEventListener('click', () => {
     modal.showModal();
