ac3fe26385c22aa648356cd924437354a9a970c2
ashleyjoseph021 <ashleyjoseph01@proton.me>
2025-08-18 00:07:45 -0600

    fixed modals

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,13 +1,49 @@
 console.log("Starting JS!")
+console.log(":3")
 
-const modal = document.querySelector('#project-modal');
-const openModal = document.querySelector(".open-button");
-const closeModal = document.querySelector("dialoge button");
+const projectCards = document.querySelectorAll('.project-card');
+console.log("\n\nPROJECT CARDS = " + projectCards);
 
-openModal.addEventListener('click', () => {
+
+for (let index = 0; index < projectCards.length; index++) {
+  const projectCard = projectCards[index];
+  // console.log("\n\nPROJECT CARD #" + index + " = " + projectCard);
+  const openBtn = projectCard.querySelector('button.open-button');
+  // console.log("\n\nOPEN BUTTON #" + index + " = " + openBtn);
+  const modal = projectCard.querySelector('dialog.modal');
+  // console.log("\n\nDIALOG #" + index + " = " + modal);
+  const closeBtn = modal.querySelector('button.close-button');
+  // console.log("\n\nCLOSE BUTTON #" + index + " = " + closeBtn);
+  
+  modal.addEventListener('click', function(e) {
+    if (e.target === e.currentTarget) {
+      e.stopPropagation();
+      console.log("Closing Modal")
+      modal.close();
+    }
+  })
+
+  openBtn.addEventListener('click', () => {
+    console.log("Opening Modal");
     modal.showModal();
-})
+  })
 
-closeModal.addEventListener('click', () => {
+  closeBtn.addEventListener('click', () => {
+    console.log("Closing Modal");
     modal.close();
-})
+  })
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
