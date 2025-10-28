3b796987c477069daeda092446bdf545333bede3
ashleyjoseph021 <ashleyjoseph01@proton.me>
2025-10-27 22:41:34 -0600

    modals finished

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,40 +1,44 @@
 console.log("Starting JS!")
 console.log(":3")
 
-const projectCards = document.querySelectorAll('.project-card');
-console.log("\n\nPROJECT CARDS = " + projectCards);
+document.addEventListener("DOMContentLoaded", () => {
 
-
-for (let index = 0; index < projectCards.length; index++) {
-  const projectCard = projectCards[index];
-  // console.log("\n\nPROJECT CARD #" + index + " = " + projectCard);
-  const openBtn = projectCard.querySelector('button.open-button');
-  // console.log("\n\nOPEN BUTTON #" + index + " = " + openBtn);
-  const modal = projectCard.querySelector('dialog.modal');
-  // console.log("\n\nDIALOG #" + index + " = " + modal);
-  const closeBtn = modal.querySelector('button.close-button');
-  // console.log("\n\nCLOSE BUTTON #" + index + " = " + closeBtn);
+  const projectCards = document.querySelectorAll('.project-card');
+  console.log("\n\nPROJECT CARDS = " + projectCards);
   
-  modal.addEventListener('click', function(e) {
-    if (e.target === e.currentTarget) {
-      e.stopPropagation();
-      console.log("Closing Modal")
+  
+  for (let index = 0; index < projectCards.length; index++) {
+    const projectCard = projectCards[index];
+    console.log("\n\nPROJECT CARD #" + index + " = " + projectCard);
+    const openBtn = projectCard.querySelector('button.open-button');
+    console.log("\n\nOPEN BUTTON #" + index + " = " + openBtn);
+    const modal = projectCard.querySelector('dialog.modal');
+    console.log("\n\nDIALOG #" + index + " = " + modal);
+    const closeBtn = modal.querySelector('button.close-button');
+    console.log("\n\nCLOSE BUTTON #" + index + " = " + closeBtn);
+    
+    modal.addEventListener('click', function(e) {
+      if (e.target === e.currentTarget) {
+        e.stopPropagation();
+        console.log("Closing Modal")
+        modal.close();
+      }
+    })
+  
+    openBtn.addEventListener('click', () => {
+      console.log("Opening Modal");
+      modal.showModal();
+    })
+  
+    closeBtn.addEventListener('click', () => {
+      console.log("Closing Modal");
       modal.close();
-    }
-  })
+    })
+  
+  
+  }
+})
 
-  openBtn.addEventListener('click', () => {
-    console.log("Opening Modal");
-    modal.showModal();
-  })
-
-  closeBtn.addEventListener('click', () => {
-    console.log("Closing Modal");
-    modal.close();
-  })
-
-
-}
 
 
 
