22ace84845e746a7005590a0af6c4a3c7a70e57a
ashleyjoseph021 <ashleyjoseph01@proton.me>
2026-01-31 02:01:23 -0700

    responsive film roll

--- scripts/updateText.js
+++ scripts/updateText.js
@@ -6,9 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const ageText = document.getElementById('karzAge');
     const copyrightText = document.getElementById('copyrightYear');
     const currentDate = new Date();
+    let age = currentDate.getFullYear() - 2004;
+
+    if ((currentDate.getMonth() > 9) || (currentDate.getMonth() == 9 && currentDate.getDate() >= 28)) age++;
 
     copyrightText.innerText = currentDate.getFullYear();
-
+    ageText.innerText = age;
 })
 
 
